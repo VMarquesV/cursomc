d8e730791b6c7a20be1ae77826a3fe3fa252fddb
VMarquesV <viniciusmar2014@outlook.com>
2022-03-23 12:21:14 -0300

    Created Table Produto and Associantion Many to Many

--- src/main/java/com/example/cursomc/repositories/ProdutoRepository.java
+++ src/main/java/com/example/cursomc/repositories/ProdutoRepository.java
@@ -4,10 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 
-import com.example.cursomc.domain.Categoria;
+import com.example.cursomc.domain.Produto;
 
 
 @Repository
-public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
+public interface ProdutoRepository extends JpaRepository<Produto, Integer>{
 	
 }	
